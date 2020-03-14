e7bc329a4797bb9a3fcca47c8eb9a37925617b05
pilecki <46523870+pilecki@users.noreply.github.com>
2020-03-14 07:35:09 +0000

    Update settings.py

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
